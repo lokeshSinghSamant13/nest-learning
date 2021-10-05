58f975b64a54a70ac398bdf98345b78fae5b87bb
lokeshSinghSamantBuild <67457874+lokeshSinghSamantBuild@users.noreply.github.com>
2021-10-05 17:38:09 +0530

    basic nestjs overview covered

---
+++ src/cats/dto/create-cat.dto.ts
@@ -0,0 +1,12 @@
+export class CreateCatDto {
+  name: string;
+  age: number;
+  breed: string;
+}
+
+export class UpdateCatDto {
+  id: string;
+  name: string;
+  age: number;
+  breed: string;
+}
