e0679d7cb1209087bbfa9f5d5289b6373a160095
lokeshSinghSamantBuild <67457874+lokeshSinghSamantBuild@users.noreply.github.com>
2021-10-06 08:47:59 +0530

    configuration setting implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from 'config/configuration';
+import { validate } from 'env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+      load: [configuration],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
