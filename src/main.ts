70fad6582c04453fee286e76cbc614ee01726cd6
lokeshSinghSamantBuild <67457874+lokeshSinghSamantBuild@users.noreply.github.com>
2021-10-11 09:40:26 +0530

    basic server Implmented

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats Example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'My API Docs',
+  };
+  SwaggerModule.setup('api', app, document, customOptions);
+
   await app.listen(3000);
 }
 bootstrap();
